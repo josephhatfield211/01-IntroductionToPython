f8e7c9047b0f4806547243f17129735e8b09fe86
josephhatfield211 <jhjoeman224@gmail.com>
2018-08-30 10:48:14 -0400

    done maybe

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Joey Hatfield.
 """
 ###############################################################################
 #
-# TODO: 1.
+# done: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# done: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# done: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving, *click anywhere in the window to close it*.
 #
@@ -109,9 +109,101 @@ natasha.forward(50)
 natasha.speed = 1  # Now slower
 natasha.go_to(rg.Point(-100, 200))
 
+natasha.speed = 10  # Faster
+natasha.pen = rg.Pen('green', 10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+natasha.right(10)
+natasha.forward(10)
+
+maenard = rg.SimpleTurtle('turtle')
+
+maenard.speed = 2
+
+maenard.pen = rg.Pen('Magenta', 5)
+
+maenard.backward(100)
+maenard.right(90)
+maenard.backward(100)
+maenard.right(90)
+maenard.backward(100)
+maenard.right(90)
+maenard.backward(100)
+
+maenard.speed = 2
+
+maenard.right(420)
 ###############################################################################
 #
-# TODO: 4.
+# done: 4.
 #   Add a few more lines of your own code to make one of the
 #   existing SimpleTurtles move some more and/or have different
 #   characteristics.
@@ -125,7 +217,7 @@ natasha.go_to(rg.Point(-100, 200))
 
 ###############################################################################
 #
-# TODO: 5.
+# done: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       boris    natasha
